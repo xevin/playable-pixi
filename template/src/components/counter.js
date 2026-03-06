85569e7a0ec0abf9046c575d89e48791e63428f8
Krapivin Stepan <xevinbox@yandex.ru>
2026-03-06 18:34:27 +0300

    доработки компонента counter

--- template/src/components/counter.js
+++ template/src/components/counter.js
@@ -1,4 +1,4 @@
-import { Container, Text } from "pixi.js"
+import { Container, Sprite, Text } from "pixi.js"
 import gsap from "gsap"
 
 export default class CounterComponent extends Container {
@@ -11,19 +11,30 @@ export default class CounterComponent extends Container {
     // количество нулей после запятой
     this.zeroes = props?.zeroes ?? 0
 
-    this.style = props?.style ?? {}
-    this.background = props?.background
+    this.style = props?.style
+    this.texture = props?.texture
+    this.textureOffset = props?.textureOffset ?? {x: 0, y: 0}
     this.value = props?.value ?? 0
 
     this.valueLabel = new Text({
       text: this.getFormattedValue(),
-      style: {
-        ...this.style
-      },
-      anchor: 0.5
+      anchor: 0.5,
     })
+
+    if(this?.style) {
+      this.valueLabel.style = this.style
+    }
+
+    if (this?.texture) {
+      this.addChild(new Sprite({
+        texture: this.texture,
+        anchor: 0.5,
+        x: this.textureOffset.x,
+        y: this.textureOffset.y,
+      }))
+    }
+
     this.addChild(
-      this.background,
       this.valueLabel,
     )
   }
@@ -40,9 +51,5 @@ export default class CounterComponent extends Container {
         this.valueLabel.text = this.getFormattedValue()
       }
     })
-  }
-
-  async play() {
-    //
   }
 }
