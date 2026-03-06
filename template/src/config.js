975688bc4a8998227611aa6569595455c6855413
Krapivin Stepan <xevinbox@yandex.ru>
2026-03-06 14:42:46 +0300

    добавил настройки шрифта по-умолчанию

--- template/src/config.js
+++ template/src/config.js
@@ -1,12 +1,12 @@
+import { TextStyle } from "pixi.js"
+
+TextStyle.defaultTextStyle.fill = "#FFFFFF"
+TextStyle.defaultTextStyle.fontFamily = "Roboto"
+TextStyle.defaultTextStyle.align = "center"
+
 export default {
   width: 1920,
   height: 1080,
   backgroundColor: "#080d22",
   antialias: true,
-  defaultFontStyles: {
-    fill: "#FFFFFF",
-    fontSize: 60,
-    fontFamily: "Roboto",
-    align: "center"
-  }
 }
